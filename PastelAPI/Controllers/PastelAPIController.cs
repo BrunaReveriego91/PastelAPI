79afa08c4196d8e9a25f9ed097ef327adffac2e3
Bruna Reveriego <Bruna.Reveriego@br.ey.com>
2022-04-26 12:40:24 -0300

    primeiro commit

---
+++ PastelAPI/Controllers/PastelAPIController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PastelAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PastelAPIController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetPastelFrango")]
+        public String GetPastelFrango()
+        {
+            return "Pastel de frango com catupiry";
+        }
+
+        [HttpGet]
+        [Route("GetPastelCarne")]
+        public String GetPastelCarne()
+        {
+            return "Pastel de carne com mussarela";
+        }
+
+    }
+}
---
+++ PastelAPI/RemoverExcluirSwaggerFilter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Configuration;
+
+public class RemoverExcluirSwaggerFilter : IDocumentFilter
+{
+    private readonly IConfiguration _config;
+
+    public RemoverExcluirSwaggerFilter(IConfiguration config)
+    {
+        _config = config;
+
+    }
+
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        var rotas = swaggerDoc.Paths
+            .ToList();
+
+        rotas.ForEach(x =>
+        {
+            if (VerifyAppSettings(x) == false)
+                swaggerDoc.Paths.Remove(x.Key);
+        }
+        );
+
+
+    }
+
+    private bool VerifyAppSettings(KeyValuePair<string, OpenApiPathItem> rota)
+    {
+        var rotasPermitidas = _config.GetSection("AllowedRoutes:0").GetChildren().AsEnumerable();
+
+        foreach (var item in rotasPermitidas)
+        {
+            if (rota.Key == item.Key)
+                return Convert.ToBoolean(item.Value);
+        }
+        return true;
+    }
+
+}
